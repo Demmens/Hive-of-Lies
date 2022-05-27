31e387d0d75c2d622aecd7aae693507e91d7fa4b
Dom <60469606+Demmens@users.noreply.github.com>
2022-05-27 10:47:58 +0100

    Recoded the Buzzed Bee to work with the new cards missions. Haven't tested yet, so could be disfunctional

--- Assets/Scripts/Resources/Abilities/BuzzedBee/BuzzedBeeAbility.cs
+++ Assets/Scripts/Resources/Abilities/BuzzedBee/BuzzedBeeAbility.cs
@@ -4,15 +4,37 @@ using UnityEngine;
 
 public class BuzzedBeeAbility : RoleAbility
 {
-    /*DiceMission dice;
+    CardsMission cards;
+
+    List<Card> affectedCards = new List<Card>();
     void Start()
     {
-        dice = DiceMission.singleton;
-        dice.OnPlayerRolled += PlayerRolled;
+        cards = CardsMission.singleton;
+        cards.OnPlayCard += PlayerPlayed;
+        cards.OnDrawCard += PlayerDrew;
+        cards.OnMissionEnded += MissionEnded;
     }
 
-    void PlayerRolled(Player ply, ref PlayerRollInfo roll)
+    void PlayerDrew(Player ply, ref Card card)
     {
-        if (ply == Owner) roll.currentRoll += dice.ExhaustionPenalty * ply.Exhaustion;
-    }*/
+        if (ply == Owner)
+        {
+            card.DisplayValue += CardsMission.ExhaustionPenalty * ply.Exhaustion;
+            affectedCards.Add(card);
+        }
+    }
+
+    void PlayerPlayed(Player ply, ref Card card, ref int value)
+    {
+        if (ply == Owner)
+        {
+            value += CardsMission.ExhaustionPenalty * ply.Exhaustion;
+        }
+    }
+
+    void MissionEnded(MissionResult result)
+    {
+        affectedCards.ForEach((Card card) => { card.DisplayValue -= CardsMission.ExhaustionPenalty * Owner.Exhaustion; });
+        affectedCards = new List<Card>();
+    }
 }
