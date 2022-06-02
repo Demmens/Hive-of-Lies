97bf6bacacbd3d3d5c3ce4bb9a5829eef55673c2
Dom <60469606+Demmens@users.noreply.github.com>
2022-06-02 09:51:58 +0100

    Fumble bee no longer has an ability. Instead they start with a really bad deck of cards.

--- Assets/Scripts/Resources/Abilities/FumbleBee/FumbleBeeAbility.cs
+++ Assets/Scripts/Resources/Abilities/FumbleBee/FumbleBeeAbility.cs
@@ -4,26 +4,4 @@ using UnityEngine;
 
 public class FumbleBeeAbility : RoleAbility
 {
-    /*DiceMission dice;
-    /// <summary>
-    /// The number your roll is capped at
-    /// </summary>
-    [SerializeField] int rollCap = 5;
-    /// <summary>
-    /// The number of rolls you make with this cap enabled
-    /// </summary>
-    [SerializeField] int rollsWithCap = 2;
-    void Start()
-    {
-        dice = FindObjectOfType<DiceMission>();
-
-        dice.OnPlayerRolled += ModifyDiceRoll;
-    }
-
-    void ModifyDiceRoll(Player ply, ref PlayerRollInfo roll)
-    {
-        if (ply != Owner || roll.rerollsUsed < rollsWithCap || roll.currentRoll <= rollCap) return;
-
-        roll.currentRoll = Random.Range(1, rollCap);
-    }*/
 }
