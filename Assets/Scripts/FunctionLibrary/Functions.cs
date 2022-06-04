f784d85f53c2c0ccf1a8dc0ffdb1d089cce371ce
Dom <60469606+Demmens@users.noreply.github.com>
2022-06-04 09:49:01 +0100

    Added function to search a list of players by the ID instead of the class.

--- Assets/Scripts/FunctionLibrary/Functions.cs
+++ Assets/Scripts/FunctionLibrary/Functions.cs
@@ -4,14 +4,31 @@ using UnityEngine;
 using Mirror;
 using System.Linq;
 
-namespace HiveOfLies
+public static class Functions
 {
-    public static class Functions
+    /// <summary>
+    /// Finds the corresponding networkconnection for a given player
+    /// </summary>
+    /// <param name="ply"></param>
+    /// <returns></returns>
+    public static NetworkConnection FindConnectionFromPlayer(Player ply)
     {
-        public static NetworkConnection FindConnectionFromPlayer(Player ply)
+        return GameInfo.Players.FirstOrDefault(x => x.Value == ply).Key;
+    }
+
+    /// <summary>
+    /// Whether a list of players contains a player of the given ID
+    /// </summary>
+    /// <param name="playerList"></param>
+    /// <param name="playerID">The ID of the player to find</param>
+    /// <returns></returns>
+    public static bool Contains(this List<Player> playerList, ulong playerID) {
+        for (int i = 0; i < playerList.Count; i++)
         {
-            return GameInfo.Players.FirstOrDefault(x => x.Value == ply).Key;
+            if (playerList[i].ID == playerID) return true;
         }
+
+        return false;
     }
 }
 
