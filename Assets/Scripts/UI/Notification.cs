6d3d33570fafda73ccc16456a1591c2d555b8e90
Dom <60469606+Demmens@users.noreply.github.com>
2022-08-22 08:48:10 +0100

    Created event that fires on the server when a notification is sent. This will allow roles to interact with notifications (such as showing investigate results)

--- Assets/Scripts/UI/Notification.cs
+++ Assets/Scripts/UI/Notification.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using Mirror;
 
 public class Notification : MonoBehaviour
 {
@@ -15,20 +16,48 @@ public class Notification : MonoBehaviour
     /// </summary>
     public static Notification Singleton;
 
+    public event NotificationCreated OnNotificationCreated;
+    public delegate void NotificationCreated(string text, NotificationType type, NetworkConnection conn);
+
 
     void Start()
     {
         text = gameObject.GetComponentInChildren<TMP_Text>();
         Singleton = this;
+        NetworkServer.RegisterHandler<CreatedNotificationsMsg>(ServerReceiveNotification);
     }
 
     /// <summary>
     /// Call to change the 
     /// </summary>
     /// <param name="newText"></param>
-    public void CreateNotification(string newText)
+    public void CreateNotification(string newText, NotificationType type = NotificationType.generic)
     {
         text.text = newText;
         gameObject.SetActive(true);
+
+    }
+
+    /// <summary>
+    /// Called automatically on the server when a notification is sent out on the client.
+    /// </summary>
+    /// <param name="conn"></param>
+    /// <param name="msg"></param>
+    private void ServerReceiveNotification(NetworkConnection conn, CreatedNotificationsMsg msg)
+    {
+        OnNotificationCreated?.Invoke(msg.text, msg.type, conn);
+    }
+
+    public enum NotificationType
+    {
+        generic,
+        investigate,
+
+    }
+
+    public struct CreatedNotificationsMsg : NetworkMessage
+    {
+        public string text;
+        public NotificationType type;
     }
 }
