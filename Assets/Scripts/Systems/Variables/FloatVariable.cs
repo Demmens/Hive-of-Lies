6cf091cef2579ffde8a6074c39607bc9865c5ed8
Dom <60469606+Demmens@users.noreply.github.com>
2022-10-01 10:37:45 +0100

    Added scriptableobject variables for basic datatypes

---
+++ Assets/Scripts/Systems/Variables/FloatVariable.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Float", menuName = "Variable/Float")]
+public class FloatVariable : ScriptableObject
+{
+    public float value;
+}
---
+++ Assets/Scripts/Systems/Variables/StringVariable.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "String", menuName = "Variable/String")]
+public class StringVariable : ScriptableObject
+{
+    public string value;
+}
