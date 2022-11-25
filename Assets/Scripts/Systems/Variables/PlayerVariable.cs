53bb61b26d46442602f5c4339febdf8780b43c1d
Dom <60469606+Demmens@users.noreply.github.com>
2022-11-25 19:49:38 +0000

    Created MissionVariable, PlayerVariable, and UlongVariable

---
+++ Assets/Scripts/Systems/Variables/PlayerVariable.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Player", menuName = "Variable/Classes/Player")]
+public class PlayerVariable : Variable<Player>
+{
+}
