136c30be688dfb492d8c0649aaab3548912555a7
Dom <60469606+Demmens@users.noreply.github.com>
2022-11-27 11:37:36 +0000

    Added ++ and -- operators for the IntVariable class

--- Assets/Scripts/Systems/Variables/IntVariable.cs
+++ Assets/Scripts/Systems/Variables/IntVariable.cs
@@ -5,4 +5,13 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "Int", menuName = "Variable/Basic/Int")]
 public class IntVariable : Variable<int>
 {
+    public static IntVariable operator ++(IntVariable a)
+    {
+        a.Value++;
+        return a;
+    }
+    public static IntVariable operator --(IntVariable a) {
+        a.Value--;
+        return a;
+    }
 }
