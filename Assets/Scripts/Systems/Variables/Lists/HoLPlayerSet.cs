68dc1932042ad414f2bc7e1a90ba118df8442e05
Dom <60469606+Demmens@users.noreply.github.com>
2022-12-03 15:59:10 +0000

    Added HoLPlayerSet

---
+++ Assets/Scripts/Systems/Variables/Lists/HoLPlayerSet.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "HoL Player List", menuName = "Variable/Sets/HoL Player")]
+public class HoLPlayerSet : RuntimeSet<HoLPlayer>
+{
+}
