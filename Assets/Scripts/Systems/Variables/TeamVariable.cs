7d78ee68d1ec9eb7e48510c16a6f2e872fe53435
Dom <60469606+Demmens@users.noreply.github.com>
2022-12-03 15:58:46 +0000

    Reorganised folders for variables

--- Assets/Scripts/Systems/Variables/TeamVariable.cs
+++
@@ -1,8 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "Team", menuName = "Variable/Enum/Team")]
-public class TeamVariable : Variable<Team>
-{
-}
