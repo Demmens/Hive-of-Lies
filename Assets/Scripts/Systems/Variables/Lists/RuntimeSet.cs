3ac2ddd765be7f31e166e88580778672e911594a
Dom <60469606+Demmens@users.noreply.github.com>
2022-12-03 15:59:01 +0000

    Added runtime sets

---
+++ Assets/Scripts/Systems/Variables/Lists/RuntimeSet.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class RuntimeSet<T> : Variable<List<T>>
+{
+    public delegate void ListChanged(T item);
+    public event ListChanged BeforeItemAdded;
+    public event ListChanged AfterItemAdded;
+
+    public event ListChanged BeforeItemRemoved;
+    public event ListChanged AfterItemRemoved;
+
+    public void Add(T item) {
+        if (Value.Contains(item)) return;
+
+        BeforeItemAdded.Invoke(item);
+        Value.Add(item);
+        AfterItemAdded.Invoke(item);
+    }
+
+    public void Remove(T item)
+    {
+        if (!Value.Contains(item)) return;
+
+        BeforeItemRemoved.Invoke(item);
+        Value.Remove(item);
+        AfterItemRemoved.Invoke(item);
+    }
+}
