2ad1114449b52732aaa7da8ecf0a8fe52ae5a222
Dom <60469606+Demmens@users.noreply.github.com>
2022-12-12 21:38:51 +0000

    Added new necessary variables to HoLPlayer.cs

--- Assets/Scripts/Systems/HoLPlayer.cs
+++ Assets/Scripts/Systems/HoLPlayer.cs
@@ -15,37 +15,54 @@ public class HoLPlayer : NetworkBehaviour
     /// Returns true on the client that is represented by this object.
     /// </summary>
     public bool LocalPlayer => hasAuthority && NetworkClient.active;
+
+    /// <summary>
+    /// The NetworkConnection associated with this player
+    /// </summary>
+    public NetworkConnection connection;
     #endregion
 
     #region Gameplay
     /// <summary>
     /// The team of this player. Whether they are a bee or a wasp.
     /// </summary>
+    [HideInInspector]
     public TeamVariable Team = new TeamVariable();
 
     /// <summary>
     /// The game's currency. Used for redrawing cards, placing extra votes, and running for team leader.
     /// </summary>
+    [HideInInspector]
     public IntVariable Favour = new IntVariable();
 
     /// <summary>
     /// The role that the player has. This determines their deck of cards, starting favour, and special ability.
     /// </summary>
+    [HideInInspector]
     public RoleVariable Role = new RoleVariable();
 
     /// <summary>
     /// How many missions the player has been on in a row. Each level reduces the value of the cards in their deck.
     /// </summary>
+    [HideInInspector]
     public IntVariable Exhaustion = new IntVariable();
 
     /// <summary>
     /// How much favour it costs to draw another card
     /// </summary>
+    [HideInInspector]
     public IntVariable NextDrawCost = new IntVariable();
 
     /// <summary>
     /// How much favour it costs to place another vote
     /// </summary>
+    [HideInInspector]
     public IntVariable NextVoteCost = new IntVariable();
+
+    /// <summary>
+    /// List of the choices of roles the player will have at the start of the game
+    /// </summary>
+    [HideInInspector]
+    public List<RoleData> RoleChoices = new List<RoleData>();
     #endregion
 }
