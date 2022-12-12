cb150f4d789eccfa7d04b7004a754944dd678707
Dom <60469606+Demmens@users.noreply.github.com>
2022-12-12 22:01:33 +0000

    Added a new runtime set for roles

---
+++ Assets/Scripts/Systems/Variables/Lists/RoleSet.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Role Set", menuName = "Variable/Sets/Role")]
+public class RoleSet : RuntimeSet<Role>
+{
+}
