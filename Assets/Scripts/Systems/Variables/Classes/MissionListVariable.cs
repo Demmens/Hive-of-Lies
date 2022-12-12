7e3bbb67f3dfa6bebdf12ec2791913e1e1a4973c
Dom <60469606+Demmens@users.noreply.github.com>
2022-12-12 22:41:03 +0000

    Added MissionListVariable.cs

---
+++ Assets/Scripts/Systems/Variables/Classes/MissionListVariable.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Mission List", menuName = "Variable/Classes/Mission List")]
+public class MissionListVariable : Variable<MissionList>
+{
+}
