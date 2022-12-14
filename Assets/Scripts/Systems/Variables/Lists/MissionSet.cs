c276b4686f136a71d71f3a5a49c58e239cd7ce4b
Dom <60469606+Demmens@users.noreply.github.com>
2022-12-14 19:29:19 +0000

    Added MissionSet.cs

---
+++ Assets/Scripts/Systems/Variables/Lists/MissionSet.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Mission Set", menuName = "Variable/Sets/Mission")]
+public class MissionSet : RuntimeSet<Mission>
+{
+}
