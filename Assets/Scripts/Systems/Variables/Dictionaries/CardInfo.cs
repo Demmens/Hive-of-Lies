b45e44696a617bc6087c2fa246913b457e8d75b2
Dom <60469606+Demmens@users.noreply.github.com>
2022-12-15 20:01:48 +0000

    Created a new Dictionary variable for storing the decks of every player

---
+++ Assets/Scripts/Systems/Variables/Dictionaries/CardInfo.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+
+[CreateAssetMenu(fileName = "CardInfo", menuName = "Variable/Dictionaries/HoLPlayer -> Deck")]
+public class CardInfo : Variable<Dictionary<HoLPlayer, Deck>>
+{
+}
