f89222b56b7c7f743c94a93b6d9804e149cf9150
Dom <60469606+Demmens@users.noreply.github.com>
2022-12-17 11:52:28 +0000

    CostCalculation.cs reworked to use the new ScriptableObject system

--- Assets/Scripts/Systems/CostCalculation.cs
+++ Assets/Scripts/Systems/CostCalculation.cs
@@ -6,64 +6,22 @@ using Mirror;
 
 public class CostCalculation : NetworkBehaviour
 {
-    public static CostCalculation singleton;
-    /// <summary>
-    /// Number of rolls the player gets to make for free. Should always be at least 1.
-    /// </summary>
-    [SyncVar] public int FreeRolls = 2;
-    /// <summary>
-    /// Cost of the first paid reroll
-    /// </summary>
-    [SyncVar] public int FirstRerollCost = 4;
-    /// <summary>
-    /// How much to increase all roll costs by
-    /// </summary>
-    [SyncVar] public int GlobalRollCostMod = 0;
+    [Tooltip("Number of draws the player gets to make for free. Should always be at least 1.")]
+    [SerializeField] IntVariable freeDraws;
 
-    /// <summary>
-    /// Number times the player may draw a card for free. Should always be at least 1.
-    /// </summary>
-    [SyncVar] public int FreeDraws = 2;
+    [Tooltip("Cost of the first paid draw.")]
+    [SerializeField] IntVariable firstDrawCost;
 
-    /// <summary>
-    /// Cost of the first paiud draw
-    /// </summary>
-    [SyncVar] public int FirstDrawCost = 4;
-
-    /// <summary>
-    /// How much to increase all draw costs by
-    /// </summary>
-    [SyncVar] public int GlobalDrawCostMod = 0;
-
-    /// <summary>
-    /// Sends vote cost information out for modification
-    /// </summary>
-    public event VoteCalculation OnVoteCalculation;
-    public delegate void VoteCalculation(CSteamID ply, ref int cost);
-
-    /// <summary>
-    /// Sends roll cost information out for modification
-    /// </summary>
-    public event RerollCalculation OnRerollCalculation;
-    public delegate void RerollCalculation(CSteamID ply, ref int cost);
-
-    /// <summary>
-    /// Sends draw cost information out for modification
-    /// </summary>
-    public event DrawCalculation OnDrawCalculation;
-    public delegate void DrawCalculation(ulong ply, ref int cost);
-
-    void Start()
-    {
-        singleton = this;
-    }
+    [Tooltip("How much to increase all draw costs by")]
+    [SerializeField] IntVariable globalDrawCostMod;
 
     /// <summary>
     /// Calculate the favour cost of the vote
     /// </summary>
     /// <param name="ply">The player who's voting</param>
     /// <param name="numVotes">The number of votes</param>
-    public int CalculateVoteCost(CSteamID ply, int numVotes)
+    [Command(requiresAuthority = false)]
+    public int CalculateVoteCost(int numVotes)
     {
         //Votes cost the same up and down
         numVotes = Mathf.Abs(numVotes);
@@ -72,35 +30,7 @@ public class CostCalculation : NetworkBehaviour
         // Cumulatively: {0,2,6,12,20}
         int cost = Mathf.Max(0, (numVotes - 1) * 2);
 
-        OnVoteCalculation?.Invoke(ply, ref cost);
-
         return cost;
-    }
-
-    /// <summary>
-    /// Calculates the cost of the roll
-    /// </summary>
-    /// <param name="ply">The player rolling</param>
-    /// <param name="numRerolls">The number of rolls they have done so far</param>
-    /// <returns></returns>
-    public int CalculateRerollCost(CSteamID ply, int numRolls)
-    {
-        //Make sure the correct number of rerolls are free
-        if (numRolls < FreeRolls) return 0;
-
-        //Calculate how many rerolls we have used that aren't free.
-        numRolls -= FreeRolls;
-
-        //Formula. Can edit this however we like for balance.
-        int cost = FirstRerollCost * (numRolls + 1);
-
-        //Currently deciding to put this before roles tinker with it. Might change later, who knows.
-        cost += GlobalRollCostMod;
-
-        //Allow listeners to modify the cost
-        OnRerollCalculation?.Invoke(ply, ref cost);
-
-        return Mathf.Max(cost, 0);
     }
 
     /// <summary>
@@ -109,22 +39,20 @@ public class CostCalculation : NetworkBehaviour
     /// <param name="ply">The player drawing a card</param>
     /// <param name="numRerolls">The number of cards they have drawn so far</param>
     /// <returns></returns>
-    public int CalculateDrawCost(ulong ply, int numDraws)
+    [Command(requiresAuthority = false)]
+    public int CalculateDrawCost(int numDraws)
     {
         //Make sure the correct number of rerolls are free
-        if (numDraws < FreeDraws) return 0;
+        if (numDraws < freeDraws) return 0;
 
         //Calculate how many rerolls we have used that aren't free.
-        numDraws -= FreeDraws;
+        numDraws -= freeDraws;
 
         //Formula. Can edit this however we like for balance.
-        int cost = FirstRerollCost * (numDraws + 1);
+        int cost = firstDrawCost * (numDraws + 1);
 
         //Currently deciding to put this before roles tinker with it. Might change later, who knows.
-        cost += GlobalDrawCostMod;
-
-        //Allow listeners to modify the cost
-        OnDrawCalculation?.Invoke(ply, ref cost);
+        cost += globalDrawCostMod;
 
         return Mathf.Max(cost, 0);
     }
