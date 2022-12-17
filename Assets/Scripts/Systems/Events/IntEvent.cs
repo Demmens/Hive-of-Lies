12c3bc5643b369ca777929d18f3b9e14b3e255b6
Dom <60469606+Demmens@users.noreply.github.com>
2022-12-17 18:52:48 +0000

    Created IntEvent and the corresponding listener

---
+++ Assets/Scripts/Systems/Events/IntEvent.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Int Event", menuName = "Events/Int")]
+public class IntEvent : GameEvent<int>
+{
+}
