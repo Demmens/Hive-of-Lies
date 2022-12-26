d28748d05c1a1e4adaeff24783456e4ff5072b32
Dom <60469606+Demmens@users.noreply.github.com>
2022-12-26 19:56:10 +0000

    Added ulong events and ulong event listeners

---
+++ Assets/Scripts/Systems/Events/UlongEvent.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Int Event", menuName = "Events/Ulong")]
+public class UlongEvent : GameEvent<ulong>
+{
+}
