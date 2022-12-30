982f920a2d0fd0da663e66ef5d0ae9900d92a6d9
Dom <60469606+Demmens@users.noreply.github.com>
2022-12-30 14:11:11 +0000

    Created notification script for the generic popup

--- Assets/Scripts/UI/Notification.cs
+++ Assets/Scripts/UI/Notification.cs
@@ -9,76 +9,22 @@ public class Notification : MonoBehaviour
     /// <summary>
     /// The text of the notification
     /// </summary>
-    private TMP_Text text;
+    [SerializeField] TMP_Text text;
 
     /// <summary>
-    /// The only notification script in the scene 
-    /// </summary>
-    public static Notification Singleton;
-
-    /// <summary>
-    /// Invoked when a notification is created
-    /// </summary>
-    public event NotificationCreated OnNotificationCreated;
-    public delegate void NotificationCreated(string text, NotificationType type, NetworkConnection conn);
-
-
-    void Start()
-    {
-        text = gameObject.GetComponentInChildren<TMP_Text>();
-        Singleton = this;
-        NetworkServer.RegisterHandler<CreatedNotificationMsg>(ServerReceiveNotification);
-        NetworkClient.RegisterHandler<CreatedNotificationMsg>(ClientReceiveNotification);
-    }
-
-    /// <summary>
-    /// Creates a notification. If called on the server, all clients receive the notification.
+    /// Sets the notification text
     /// </summary>
     /// <param name="newText"></param>
-    public void CreateNotification(string newText, NotificationType type = NotificationType.Generic)
+    public void SetText(string newText)
     {
-        CreatedNotificationMsg msg = new CreatedNotificationMsg() { text = newText, type = type };
-        if (NetworkServer.active)
-        {
-            NetworkServer.SendToAll(msg);
-        }
-        else
-        {
-            text.text = newText;
-            gameObject.SetActive(true);
-            NetworkClient.Send(msg);
-        }
+        text.text = newText;
     }
 
     /// <summary>
-    /// If the server sends a notification, client reads it here
+    /// Destroys the notification
     /// </summary>
-    /// <param name="msg"></param>
-    private void ClientReceiveNotification(CreatedNotificationMsg msg)
+    public void CloseNotification()
     {
-        CreateNotification(msg.text, msg.type);
-    }
-
-    /// <summary>
-    /// Called automatically on the server when a notification is sent out on the client.
-    /// </summary>
-    /// <param name="conn"></param>
-    /// <param name="msg"></param>
-    private void ServerReceiveNotification(NetworkConnection conn, CreatedNotificationMsg msg)
-    {
-        OnNotificationCreated?.Invoke(msg.text, msg.type, conn);
-    }
-
-    public enum NotificationType
-    {
-        Generic,
-        Investigate,
-
-    }
-
-    public struct CreatedNotificationMsg : NetworkMessage
-    {
-        public string text;
-        public NotificationType type;
+        Destroy(gameObject);
     }
 }
