973a140fcaaf6ee5087c2f9c66c654627b655c1a
Dom <60469606+Demmens@users.noreply.github.com>
2022-12-30 16:37:52 +0000

    Finally Deleted GameInfo.cs for good.

---
+++ Assets/Scripts/Systems/Role/Role.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Role
+{
+    public RoleData Data;
+    public RoleAbility Ability;
+}
