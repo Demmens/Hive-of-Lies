48495f39c8b93fc2f455c0167e22f6a22f11d7ed
Dom <60469606+Demmens@users.noreply.github.com>
2023-01-31 17:06:51 +0000

    Created the Fixer role

---
+++ Assets/Scripts/Resources/Abilities/Fixer/FixerAbility.cs
@@ -0,0 +1,75 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+
+public class FixerAbility : RoleAbility
+{
+    #region SERVER
+    [SerializeField] int incrementAmount;
+    [SerializeField] HoLPlayerSet playersOnMission;
+    #endregion
+    #region CLIENT
+    [SerializeField] GameObject buttonPrefab;
+    private GameObject button;
+    [SerializeField] IntVariable favour;
+    #endregion
+    #region SHARED
+    [SerializeField] int cost;
+    #endregion
+
+    [Server]
+    public override void OnRoleGiven()
+    {
+        CreateButton();
+        Owner.Deck.Value.AfterPlay += OnSubmittedCard;
+    }
+
+    [Server]
+    public void OnMissionStart()
+    {
+        if (!playersOnMission.Value.Contains(Owner)) return;
+
+        ClientMissionStart();
+    }
+
+    [TargetRpc]
+    void ClientMissionStart()
+    {
+        button.SetActive(true);
+    }
+
+    [TargetRpc]
+    public void OnSubmittedCard(Card card)
+    {
+        button.SetActive(false);
+    }
+
+    [TargetRpc]
+    void CreateButton()
+    {
+        button = Instantiate(buttonPrefab);
+        button.SetActive(false);
+        GenericButton btn = button.GetComponent<GenericButton>();
+        btn.OnClicked += ClickedButton;
+        btn.SetText($"{cost}f: Increase Card Value");
+        btn.SetPos(new Vector3(Screen.width / 3, 80, 0));
+    }
+
+    [Client]
+    void ClickedButton()
+    {
+        if (favour < cost) return;
+        IncrementCardValue();
+    }
+
+    [Command]
+    void IncrementCardValue()
+    {
+        if (Owner.Favour < cost) return;
+        if (Owner.Deck.Value.Hand.Count == 0) return;
+        if (Owner.Deck.Value.Hand[0].TempValue == 20) return;
+        Owner.Deck.Value.Hand[0].TempValue += incrementAmount;
+        Owner.Favour.Value -= cost;
+    }
+}
