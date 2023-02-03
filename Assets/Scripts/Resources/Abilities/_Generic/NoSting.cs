e60174992e057456ec0c8aa431f7f45b06b03b2a
Dom <60469606+Demmens@users.noreply.github.com>
2023-02-03 08:51:57 +0000

    No Sting role ability now also works retroactively

--- Assets/Scripts/Resources/Abilities/_Generic/NoSting.cs
+++ Assets/Scripts/Resources/Abilities/_Generic/NoSting.cs
@@ -6,6 +6,11 @@ public class NoSting : RoleAbility
 {
     protected override void OnRoleGiven()
     {
+        if (Owner.Target.Value != null)
+        {
+            Owner.Target.Value = null;
+            return;
+        }
         Owner.Target.OnVariableChanged += RemoveTarget;
     }
 
