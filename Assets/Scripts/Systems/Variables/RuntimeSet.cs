14fe7585c562fb06627803a2c7d3d7e36bc73484
Dom <60469606+Demmens@users.noreply.github.com>
2023-02-04 15:26:16 +0000

    Subsets can now start full if the option is checked

--- Assets/Scripts/Systems/Variables/RuntimeSet.cs
+++ Assets/Scripts/Systems/Variables/RuntimeSet.cs
@@ -16,6 +16,9 @@ public abstract class RuntimeSet<T> : ScriptableObject
     [Tooltip("The set that this set is a subset of")]
     [SerializeField] private RuntimeSet<T> subsetOf;
 
+    [Tooltip("Whether this subset should start with the same value as the parent set")]
+    [SerializeField] private bool startFull;
+
     [Tooltip("The set that this set is an extension of")]
     [SerializeField] private RuntimeSet<T> extensionOf;
 
@@ -89,16 +92,16 @@ public abstract class RuntimeSet<T> : ScriptableObject
     public void OnEnable()
     {
         if (subsetOf != null && extensionOf != null) throw new System.Exception($"Set {name} cannot be both a subset and an extension");
+        if (subsetOf != null && subsetOf.extensionOf == this) throw new System.Exception($"{name} is a subset of {subsetOf}, and {subsetOf} is an extension of {name}");
+        if (extensionOf != null && extensionOf.subsetOf == this) throw new System.Exception($"{name} is an extension of {extensionOf}, and {extensionOf} is a subset of {name}");
 
         if (subsetOf != null)
         {
-            if (subsetOf.initialValue != null) currentValue.AddRange(subsetOf.initialValue);
             subsetOf.AfterCleared += () => subsetOf.AfterItemRemoved += item => Remove(item);
         }
 
         if (extensionOf != null)
         {
-            if (extensionOf.initialValue != null) currentValue.AddRange(extensionOf.initialValue);
             extensionOf.AfterCleared += () => extensionOf.AfterItemAdded += item => Add(item);
         }
     }
@@ -110,6 +113,8 @@ public abstract class RuntimeSet<T> : ScriptableObject
     {
         currentValue = new();
         if (initialValue != null) currentValue.AddRange(initialValue);
+        if (extensionOf != null && extensionOf.currentValue != null) currentValue.AddRange(extensionOf.currentValue);
+        if (subsetOf != null && startFull && subsetOf.currentValue != null) currentValue.AddRange(subsetOf.currentValue);
 
         if (AfterItemAdded != null)
         {
@@ -117,12 +122,15 @@ public abstract class RuntimeSet<T> : ScriptableObject
             {
                 AfterItemAdded -= (System.Action<T>)d;
             }
+        }
+
+        if (AfterItemRemoved != null) {
             foreach (System.Delegate d in AfterItemRemoved.GetInvocationList())
             {
                 AfterItemRemoved -= (System.Action<T>)d;
             }
         }
-        
+
         AfterCleared?.Invoke();
     }
 
@@ -133,5 +141,7 @@ public abstract class RuntimeSet<T> : ScriptableObject
         if (Application.isPlaying) return;
         currentValue = new();
         if (initialValue != null) currentValue.AddRange(initialValue);
+        if (extensionOf != null) currentValue.AddRange(extensionOf.currentValue);
+        if (subsetOf != null && startFull) currentValue.AddRange(subsetOf.currentValue);
     }
 }
