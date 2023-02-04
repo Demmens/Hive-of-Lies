8c2bd1bde09e5a154746dfcc2170842fb5b1792d
Dom <60469606+Demmens@users.noreply.github.com>
2023-02-04 12:50:00 +0000

    Mercenary no longer gains favour as the team leader

--- Assets/Scripts/Resources/Abilities/Mercenary/MercenaryAbility.cs
+++ Assets/Scripts/Resources/Abilities/Mercenary/MercenaryAbility.cs
@@ -7,9 +7,12 @@ public class MercenaryAbility : RoleAbility
 {
     [SerializeField] int favourGain = 5;
     [SerializeField] HoLPlayerSet playersOnMission;
+    [SerializeField] HoLPlayerVariable teamLeader;
     
     public void ChoicesLockedIn()
     {
-        if (playersOnMission.Value.Contains(Owner)) Owner.Favour.Value += favourGain;
+        if (Owner == teamLeader.Value) return;
+        if (!playersOnMission.Value.Contains(Owner)) return;
+        Owner.Favour.Value += favourGain;
     }
 }
