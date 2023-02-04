6f62cb79eaf00ccde3d679a0621deacd4fbbba61
Dom <60469606+Demmens@users.noreply.github.com>
2023-02-04 13:00:46 +0000

    Created the Provocateur role

---
+++ Assets/Scripts/Resources/Abilities/Provocateur/ProvocateurAbility.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class ProvocateurAbility : RoleAbility
+{
+    [SerializeField] int favourGain = 5;
+    [SerializeField] HoLPlayerSet playersOnMission;
+    [SerializeField] VoteSet votes;
+
+    public void ChoicesLockedIn()
+    {
+        if (votes.Value.Count(vote => vote.votes < 0) == 0) return;
+        if (!playersOnMission.Value.Contains(Owner)) return;
+
+        Owner.Favour.Value += favourGain;
+    }
+}
