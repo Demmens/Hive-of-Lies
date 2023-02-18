d6821b986a48adce02f5021c6b1209ea07453358
Dom <60469606+Demmens@users.noreply.github.com>
2023-02-18 10:26:05 +0000

    Fixed plot point in set having a default name of "Greater Than"

--- Assets/Scripts/Systems/Mission/Conditions/PlotPointInSet.cs
+++ Assets/Scripts/Systems/Mission/Conditions/PlotPointInSet.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[CreateAssetMenu(fileName = "Greater Than", menuName = "Missions/Conditions/Item in Set/Mission Plot Point")]
+[CreateAssetMenu(fileName = "Plot Point in Set", menuName = "Missions/Conditions/Item in Set/Mission Plot Point")]
 public class PlotPointInSet : MissionCondition
 {
     [SerializeField] EMissionPlotPoint plotPoint;
