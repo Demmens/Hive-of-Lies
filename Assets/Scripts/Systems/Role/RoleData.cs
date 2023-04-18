3c17d21d9b6deec6131226f481d2140c210a56e8
Dom <60469606+Demmens@users.noreply.github.com>
2023-04-18 13:25:21 +0100

    Changed starting deck to center around 0 to keep numbers smaller but add more variance.

--- Assets/Scripts/Systems/Role/RoleData.cs
+++ Assets/Scripts/Systems/Role/RoleData.cs
@@ -35,7 +35,7 @@ public class RoleData : ScriptableObject
     /// </summary>
     [SerializeField] List<int> startingDeck = new List<int>
     {
-        1,2,2,2,3,3,3,3,3,3,4,4,4,4,4,4,5,5,5,6
+        -5,-4,-4,-3,-3,-2,-2,-1,-1,0,0,1,1,2,2,3,3,4,4,5
     };
 
     /// <summary>
