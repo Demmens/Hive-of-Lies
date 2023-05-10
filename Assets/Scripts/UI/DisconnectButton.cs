0950799f3c39e93ddd0835a5b8b60fc81fd83775
Dom <60469606+Demmens@users.noreply.github.com>
2023-05-10 13:32:35 +0100

    Removed SceneVariable as it wasn't working properly

--- Assets/Scripts/UI/DisconnectButton.cs
+++ Assets/Scripts/UI/DisconnectButton.cs
@@ -7,12 +7,12 @@ using UnityEngine.SceneManagement;
 
 public class DisconnectButton : MonoBehaviour
 {
-    [SerializeField] SceneVariable MenuScene;
+    [SerializeField] [Scene] string MenuScene;
     public void Click()
     {
         SteamMatchmaking.LeaveLobby(SteamLobby.LobbyID);
         NetworkManager.singleton.StopClient();
         NetworkManager.singleton.StopServer();
-        SceneManager.LoadScene(MenuScene.Value);
+        SceneManager.LoadScene(MenuScene);
     }
 }
