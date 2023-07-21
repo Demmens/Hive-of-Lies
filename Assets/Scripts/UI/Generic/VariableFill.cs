0f5684c4410519f1a5b1c66c20c7b7b99c51c581
Dom <60469606+Demmens@users.noreply.github.com>
2023-07-21 17:06:00 +0100

    Added lerp to the wasp facts and honey stolen bars

--- Assets/Scripts/UI/Generic/VariableFill.cs
+++ Assets/Scripts/UI/Generic/VariableFill.cs
@@ -11,8 +11,11 @@ public class VariableFill : NetworkBehaviour
     [SerializeField] bool useVariableForMax;
     [SerializeField] IntVariable varMax;
     [SerializeField] int intMax;
+    [SerializeField] float secondsToFill;
 
     [SerializeField] Image image;
+
+    bool isFilling;
 
 
     void Start()
@@ -24,6 +27,27 @@ public class VariableFill : NetworkBehaviour
     [ClientRpc]
     void OnVariableChange()
     {
-        image.fillAmount = (float)var.Value / (useVariableForMax ? varMax.Value : intMax);
+        float max = (float)var.Value / (useVariableForMax ? varMax.Value : intMax);
+        StartCoroutine(Fill(image.fillAmount, max));
+    }
+
+    IEnumerator Fill(float initialValue, float maxValue)
+    {
+        //If it's currently filling, wait for it to finish
+        while (isFilling)
+        {
+            yield return null;
+        }
+
+        float time = 0;
+        isFilling = true;
+        
+        while (time < secondsToFill)
+        {
+            time += Time.deltaTime;
+            float t = time / secondsToFill;
+            image.fillAmount = initialValue + ((maxValue - initialValue) * t);
+            yield return null;
+        }
     }
 }
