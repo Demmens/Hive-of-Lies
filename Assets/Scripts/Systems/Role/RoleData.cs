4e19773ba84556f16ecaa36c1f384dbfc74225d7
Dom <60469606+Demmens@users.noreply.github.com>
2023-07-22 12:51:40 +0100

    Reworked the editor serialisation of RoleData

--- Assets/Scripts/Systems/Role/RoleData.cs
+++ Assets/Scripts/Systems/Role/RoleData.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEditor;
 
 /// <summary>
 /// Information about the role
@@ -10,40 +11,9 @@ public class RoleData : ScriptableObject
 {
     #region Private Properties
     /// <summary>
-    /// Private counterpart to <see cref="RoleName"/>
-    /// </summary>
-    [SerializeField] string roleName;
-
-    /// <summary>
-    /// Private counterpart to <see cref="Description"/>
-    /// </summary>
-    [TextArea]
-    [SerializeField] string description;
-
-    /// <summary>
-    /// Private counterpart to <see cref="Sprite"/>
-    /// </summary>
-    [SerializeField] string sprite;
-
-    /// <summary>
-    /// Private counterpart to <see cref="StartingFavour"/>
-    /// </summary>
-    [SerializeField] int startingFavour;
-
-    /// <summary>
     /// Private counterpart to <see cref="StartingDeck"/>
     /// </summary>
     [SerializeField] List<Card> startingDeck;
-
-    /// <summary>
-    /// Private counterpart to <see cref="Team"/>
-    /// </summary>
-    [SerializeField] Team team;
-
-    /// <summary>
-    /// Private counterpart to <see cref="PlayersRequired"/>
-    /// </summary>
-    [SerializeField] int playersRequired;
 
     /// <summary>
     /// Prefab containing RoleAbility scripts
@@ -51,118 +21,96 @@ public class RoleData : ScriptableObject
     [SerializeField] List<GameObject> abilities;
 
     /// <summary>
-    /// Private counterpart of <see cref="Enabled"/>
-    /// </summary>
-    [SerializeField] bool enabled = true;
-    #endregion
-
-    #region Public Properties
-
-    /// <summary>
     /// Name of the role
     /// </summary>
-    public string RoleName
-    {
-        get
-        {
-            return roleName;
-        }
-    }
+    public string RoleName { get; private set; }
 
     /// <summary>
     /// Description for the role
     /// </summary>
-    public string Description
-    {
-        get
-        {
-            return description;
-        }
-    }
+    public string Description { get; private set; }
 
     /// <summary>
     /// Sprite used for the role UI
     /// </summary>
-    public string Sprite
-    {
-        get
-        {
-            return sprite;
-        }
-    }
+    public Sprite Sprite { get; private set; }
 
     /// <summary>
     /// Amount of influence the role starts with
     /// </summary>
-    public int StartingFavour
-    {
-        get
-        {
-            return startingFavour;
-        }
-    }
+    public int StartingFavour { get; private set; }
 
     /// <summary>
     /// The team the role belongs to
     /// </summary>
-    public Team Team
-    {
-        get
-        {
-            return team;
-        }
-    }
+    public Team Team { get; private set; }
+
+    /// <summary>
+    /// The type of wasp role this is
+    /// </summary>
+    public WaspType WaspType { get; private set; }
 
     /// <summary>
     /// The minimum number of players required in the lobby for this role to appear
     /// </summary>
-    public int PlayersRequired
-    {
-        get
-        {
-            return playersRequired;
-        }
-    }
+    public int PlayersRequired { get; private set; }
 
     /// <summary>
     /// The logic for the roles ability
     /// </summary>
-    public List<GameObject> Abilities
-    {
-        get
-        {
-            return abilities;
-        }
-    }
+    public List<GameObject> Abilities { get; private set; }
 
     /// <summary>
     /// Whether the role should appear in games
     /// </summary>
-    public bool Enabled
-    {
-        get
-        {
-            return enabled;
-        }
-        set
-        {
-            enabled = value;
-        }
-    }
+    public bool Enabled { get; private set; }
 
     /// <summary>
     /// The roles starting deck of cards
     /// </summary>
-    public List<Card> StartingDeck
+    public List<Card> StartingDeck { get; private set; }
+    #endregion
+
+    #region Editor
+#if UNITY_EDITOR
+    [CustomEditor(typeof(RoleData))]
+    public class RoleDataEditor : Editor
     {
-        get
+        bool basicInfo = true;
+
+        public override void OnInspectorGUI()
         {
-            return startingDeck;
-        }
-        set
-        {
-            startingDeck = value;
+            RoleData role = (RoleData) target;
+
+            
+            basicInfo = EditorGUILayout.BeginFoldoutHeaderGroup(basicInfo, "Basic Info");
+
+            if (basicInfo)
+            {
+                EditorGUILayout.LabelField("Name");
+                role.RoleName = EditorGUILayout.TextField(role.RoleName);
+                EditorGUILayout.LabelField("Description");
+                role.Description = EditorGUILayout.TextArea(role.Description, GUILayout.Height(50));
+                role.Team = (Team)EditorGUILayout.EnumPopup("Team", role.Team);
+                if (role.Team == Team.Wasp) role.WaspType = (WaspType)EditorGUILayout.EnumPopup("Wasp Type", role.WaspType);
+                role.StartingFavour = EditorGUILayout.IntField("Favour", role.StartingFavour);
+                role.PlayersRequired = EditorGUILayout.IntField("Player Count", role.PlayersRequired);
+                role.Sprite = (Sprite)EditorGUILayout.ObjectField("Sprite", role.Sprite, typeof(Sprite), false);
+            }
+
+            EditorGUILayout.EndFoldoutHeaderGroup();
+
+            base.OnInspectorGUI();
+
+            role.Enabled = EditorGUILayout.Toggle("Enabled", role.Enabled);
         }
     }
+#endif
     #endregion
+}
+
+public enum WaspType
+{
+    Solitary,
+    Social
 }
