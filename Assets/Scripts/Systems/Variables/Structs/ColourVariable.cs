60b4bba7ba198af11d6681d88ab3a8f571815a14
Dom <60469606+Demmens@users.noreply.github.com>
2023-07-26 12:55:36 +0100

    Created ColourVariables to keep colours consistent across the game

---
+++ Assets/Scripts/Systems/Variables/Structs/ColourVariable.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Colour", menuName = "Variable/Structs/Colour")]
+public class ColourVariable : Variable<Color>
+{
+}
