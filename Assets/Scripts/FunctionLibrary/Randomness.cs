2a4c0b14c22f2e8b0ba203e8792af8caea8be51a
Abigail <60469606+Demmens@users.noreply.github.com>
2023-08-21 21:23:23 +0100

    Fixed the shuffle method not shuffling properly (the random value was assumed to be inclusive on both ends when it was exclusive at the top end)

--- Assets/Scripts/FunctionLibrary/Randomness.cs
+++ Assets/Scripts/FunctionLibrary/Randomness.cs
@@ -13,8 +13,8 @@ public static class Randomness
         int n = list.Count;
         while (n > 1)
         {
-            n--;
             int k = Random.Range(0, n);
+            n--;
             T value = list[k];
             list[k] = list[n];
             list[n] = value;
