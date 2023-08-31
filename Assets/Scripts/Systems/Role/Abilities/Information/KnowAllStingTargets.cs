f24b01a8e85774489009ed7413546795da9d9393
Abigail <60469606+Demmens@users.noreply.github.com>
2023-08-31 17:10:21 +0100

    Created the Blackmailer role

---
+++ Assets/Scripts/Systems/Role/Abilities/Information/KnowAllStingTargets.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+using UnityEngine.Localization;
+using Mirror;
+
+public class KnowAllStingTargets : RoleAbility
+{
+    [SerializeField] hivePlayerSet waspPlayers;
+    [SerializeField] GameObject popup;
+    [SerializeField] LocalizedString popupText;
+    [SerializeField] LocalizedString noTargetsText;
+
+    public void AfterTargetsSelected()
+    {
+        string targetString = "";
+
+        foreach (hivePlayer ply in waspPlayers.Value)
+        {
+            if (ply.Target.Value == null) continue;
+
+            string playerName = ply.Target.Value.DisplayName;
+            string roleName = ply.Target.Value.Role.Value.Data.RoleName;
+            targetString += string.Format(popupText.GetLocalizedString(), playerName, roleName);
+            targetString += "\n\n";
+        }
+        targetString.TrimEnd('\n');
+
+        if (targetString == "") targetString = noTargetsText.GetLocalizedString();
+
+        CreatePopup(targetString);
+    }
+
+    [TargetRpc]
+    void CreatePopup(string text)
+    {
+        popup = Instantiate(popup);
+        popup.GetComponent<Notification>().SetText(text);
+    }
+}
