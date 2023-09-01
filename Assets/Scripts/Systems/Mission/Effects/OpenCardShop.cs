c6d6db79d21e7bac86838d04190a3cb0febe731c
Abigail <60469606+Demmens@users.noreply.github.com>
2023-09-01 18:37:46 +0100

    Created a mission effect for opening a card shop

---
+++ Assets/Scripts/Systems/Mission/Effects/OpenCardShop.cs
@@ -0,0 +1,99 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+
+public class OpenCardShop : MissionEffectBehaviour
+{
+    [SerializeField] Transform shopContents;
+    [SerializeField] GameObject popup;
+    [SerializeField] GameObject cardDisplayPrefab;
+
+    [SerializeField] hivePlayerSet alivePlayers;
+    [SerializeField] hivePlayerDictionary playersByConnection;
+    [SerializeField] IntVariable playerCount;
+    [SerializeField] IntVariable favour;
+
+    [SerializeField] List<Card> possibleCards;
+    [SerializeField] int numCardsInShop;
+
+    Dictionary<hivePlayer, List<Card>> playerShops = new();
+    List<NetworkConnection> playersClosedShop = new();
+
+    [Server]
+    public override void OnEffectTriggered()
+    { 
+    }
+
+    public override void OnStartClient()
+    {
+        RequestCards();
+    }
+
+    [Command(requiresAuthority = false)]
+    void RequestCards(NetworkConnectionToClient conn = null)
+    {
+        foreach (hivePlayer ply in alivePlayers.Value)
+        {
+            List<Card> cardsToSend = PickCards();
+            playerShops[ply] = cardsToSend;
+            GiveClientCards(ply.connectionToClient, cardsToSend);
+        }
+    }
+
+    [TargetRpc]
+    void GiveClientCards(NetworkConnection conn, List<Card> cards)
+    {
+        Debug.Log($"Client received {cards.Count} cards");
+        foreach (Card card in cards)
+        {
+            CardDisplay cardDisplay = Instantiate(cardDisplayPrefab).GetComponent<CardDisplay>();
+            cardDisplay.transform.SetParent(shopContents);
+            cardDisplay.SetCard(card);
+            cardDisplay.ShowCost(true);
+            cardDisplay.OnClick += (card) =>
+            {
+                if (favour.Value < card.BuyValue) return;
+                BuyCard(card);
+                Destroy(cardDisplay.gameObject);
+            };
+        }
+    }
+
+    [Command(requiresAuthority = false)]
+    public void BuyCard(Card card, NetworkConnectionToClient conn = null)
+    {
+        if (!playersByConnection.Value.TryGetValue(conn, out hivePlayer ply)) return;
+        if (ply.Favour.Value < card.BuyValue) return;
+
+        ply.Favour.Value -= card.BuyValue;
+        ply.Deck.Value.PublicAddToDeck(card);
+        ply.Deck.Value.DrawPile.Shuffle();
+    }
+
+    [Client]
+    public void ShopClosed()
+    {
+        ShopClosedServer();
+    }
+
+    [Command(requiresAuthority = false)]
+    void ShopClosedServer(NetworkConnectionToClient conn = null)
+    {
+        playersClosedShop.Add(conn);
+        if (playersClosedShop.Count == playerCount.Value) EndEffect();
+    }
+
+    List<Card> PickCards()
+    {
+        List<Card> picked = new();
+
+        for (int i = 0; i < numCardsInShop; i++)
+        {
+            Card card = possibleCards.GetRandom();
+            picked.Add(card);
+        }
+
+        return picked;
+    }
+}
