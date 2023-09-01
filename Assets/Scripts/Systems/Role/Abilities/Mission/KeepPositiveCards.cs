3baa3e3fa0e97c7f06fc2a016977334b9ee1cc2a
Abigail <60469606+Demmens@users.noreply.github.com>
2023-09-01 22:59:29 +0100

    Buzzed Bee no longer keeps negative cards

--- Assets/Scripts/Systems/Role/Abilities/Mission/KeepPositiveCards.cs
+++ Assets/Scripts/Systems/Role/Abilities/Mission/KeepPositiveCards.cs
@@ -2,13 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class KeepPlayedCards : RoleAbility
+public class KeepPositiveCards : RoleAbility
 {
     public void AfterDeckConstructed()
     {
         foreach (Card card in Owner.Deck.Value.DrawPile)
         {
-            card.DestroyOnPlay = false;
+            if (card.Value > 0) card.DestroyOnPlay = false;
         }
     }
 }
